3c6c8ed00bbbf9d67357d8f3f72a32ec3449caff
toashby <tashby@live.co.uk>
2018-10-12 10:26:15 +0100

    working image save but only bmp

---
+++ Image.pro
@@ -0,0 +1,5 @@
+TARGET=Image
+SOURCES += main.cpp
+OTHER_FILES+=README.md
+QMAKE_CXX += $$system(pkg-config --cflags Magick++)
+LIBS+= $$system(pkg-config --libs Magick++)
--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <cstdlib>
-
+#include <Magick++.h>
 
 int main(){
+const size_t width=400;
+const size_t height=400;
+const size_t pixelDepth=3;
+unsigned char image[width*height*pixelDepth];
+
+for(size_t i=0; i < width*height*pixelDepth; ++i){
+    image[i] = 128;
+}
+
+Magick::Image output(width,height,"RGB",Magick::CharPixel,image);
+output.depth(16);
+output.write("test.bmp");
+
     return EXIT_SUCCESS;
 }
