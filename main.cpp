f9a130d106e84e2e51abfcc96a610fdc751ffb54
toashby <tashby@live.co.uk>
2018-10-12 10:54:44 +0100

    finished working on image

--- main.cpp
+++ main.cpp
@@ -3,17 +3,40 @@
 #include <Magick++.h>
 #include <memory>
 
-int main(){
-const size_t width=200;
-const size_t height=400;
+const size_t width=20;
+const size_t height=20;
 const size_t pixelDepth=3;
+
+void setPixel(size_t _x, size_t _y, unsigned char _r, unsigned char _g, unsigned char _b, std::unique_ptr<unsigned char []> &_image)
+{
+    auto offset=(_y*width)*3+(_x*3);
+    _image[offset] = _r;
+    _image[offset+1] = _g;
+    _image[offset+2] = _b;
+}
+
+void clearScreen(char _r, char _g, char _b, std::unique_ptr<unsigned char []> &_image)
+{
+    for(size_t i=0; i < width*height*pixelDepth; ++i){
+        setPixel(i, i, 0, 0, 0, _image);
+    }
+}
+
+int main(){
+
 std::unique_ptr<unsigned char []> image=
         std::make_unique<unsigned char []>(width*height*pixelDepth);
 
 for(size_t i=0; i < width*height*pixelDepth; ++i){
-    image[i] = 128;
+    image[i] = 255;
 }
 
+for(size_t x=0; x < width; ++x){
+    setPixel(x, x, 255, 0, 0, image);
+    setPixel(width - x, x, 255, 0, 0, image);
+}
+
+setPixel(10,10,255,0,0,image);
 Magick::Image output(width,height,"RGB",Magick::CharPixel,image.get());
 output.depth(16);
 output.write("test.bmp");
